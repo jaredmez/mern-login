ef2d982f7e84f2db2416a7b5b52ba4a28b0cfd49
Jared M <mezquitaj@gmail.com>
2021-10-25 19:10:47 -0400

    connected server to atlas DB

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-console.log('hello world')
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express();
+
+app.use(express.urlencoded());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(()=>  console.log("MongoDB connected!"))
+  .catch(err => console.log(err));
+
+app.listen(PORT, () => console.log(`Server up and running on port: ${PORT}...`));
