51fe42a7f5a0b409c112ca432112e3f590a049e2
Jared M <mezquitaj@gmail.com>
2021-10-26 11:43:57 -0400

    finished with api route setup

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
+const passport = require('passport')
+
+const users = require('./routes/api/users')
 
 const app = express();
 
@@ -9,8 +12,19 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
+// connect to MongoDB
+console.log(process.env.MONGODB_URI)
 mongoose.connect(process.env.MONGODB_URI)
   .then(()=>  console.log("MongoDB connected!"))
   .catch(err => console.log(err));
+
+//Passport middleware
+app.use(passport.initialize());
+
+//passport config
+require('./config/passport')(passport);
+
+//routes
+app.use('/api/users', users);
 
 app.listen(PORT, () => console.log(`Server up and running on port: ${PORT}...`));
