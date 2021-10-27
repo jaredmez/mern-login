851465f3dd32ec48e443a5a5f33d926f67bb656a
Jared M <mezquitaj@gmail.com>
2021-10-27 17:23:28 -0400

    setup redux store and associated actions for authentication

--- client/src/App.js
+++ client/src/App.js
@@ -4,19 +4,25 @@ import Navbar from './components/layout/Navbar'
 import Landing from './components/layout/Landing'
 import Login from './components/auth/Login'
 import Register from './components/auth/Register';
+import { Provider } from 'react-redux'
+import store from './store'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <Route exact path="/" component={Landing} />
-        <Route exact path="/register" component={Register} />
-        <Route exact path="/login" component={Login} />
-    </div>
-    </Router>
-  );
+class App extends Component {
+  render(){
+    return (
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
+        </Router>
+      </Provider>
+    );
+  }
 }
 
 export default App;
