09932626da45b4e4095546010551f72f1b4a53df
Jared M <mezquitaj@gmail.com>
2021-10-27 19:50:19 -0400

    finished with login and auth setup, and started private routes

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -27,7 +27,7 @@ export const loginUser = userData => dispatch => {
     .then(res => {
       // Save to localStorage
 
-// Set token to localStorage
+      // Set token to localStorage
       const { token } = res.data;
       localStorage.setItem("jwtToken", token);
       // Set token to Auth header
